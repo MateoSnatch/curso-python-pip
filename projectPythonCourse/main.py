80c78026275f6b79030732a6f8cee5eb6bc8a959
Mateo Patino <matepatibe@gmail.com>
2024-07-29 19:39:35 -0500

    Implementacion de docker en el proyecto projectPythonCourse, nuevos archivos con la creación de una imagen docker y la ejecución del servicio. Actualización del archivo main con una mejor definición de source para el csv

--- projectPythonCourse/main.py
+++ projectPythonCourse/main.py
@@ -2,7 +2,7 @@ import read_csv
 import my_functions
 import numpy as np
 
-data = read_csv.read('/home/mateo/Platzi/cursoPythonPipYEntornosVirtuales/projectPythonCourse/data.csv')
+data = read_csv.read('./data.csv')
 country = my_functions.get_country(data)
 country_population = my_functions.get_population(country[0])
 plot = my_functions.plot(country_population[0], country_population[1], country[1])
